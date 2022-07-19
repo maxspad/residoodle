57bcf2c4c93e14333c39577b74817ca94e438b84
Maxwell Spadafore <maxspad@umich.edu>
2022-07-19 13:58:09 -0400

    added navbar

--- src/app.py
+++ src/app.py
@@ -1,8 +1,8 @@
 '''Main entrypoint for the application'''
 
 import streamlit as st
-# from streamlit_option_menu import option_menu
-import sched_consts
+from streamlit_option_menu import option_menu
+import config
 # import sched_helpers
 import sched
 # import feedback
@@ -10,12 +10,12 @@ import sched
 
 # This command can only be run once per streamlit app
 st.set_page_config(
-    page_title=sched_consts.APP_TITLE,
+    page_title=config.APP_TITLE,
     page_icon="🦝",
     menu_items={
         'Get Help':None,
         "Report a Bug":None,
-        "About":sched_consts.ABOUT_MARKDOWN
+        "About":config.ABOUT_MARKDOWN
     }
 )
 # Display the header
@@ -24,25 +24,25 @@ html = f'''
 <hr style='margin: 0.25em'/>
 <h1 style='text-align: center'>
     <img height="70px" src="data:image/png;base64,{raccoon_string}"/>
-    <span style='margin-left: 0.5em'>{sched_consts.APP_TITLE}</span>
+    <span style='margin-left: 0.5em'>{config.APP_TITLE}</span>
 </h1>
 <hr style='margin: 0.25em'  />
 '''
 st.write(html, unsafe_allow_html=True)
 
-# # Display the Navigation Bar
-# nav_bar_selected = option_menu(None, sched_consts.APP_PAGES,
-#     icons=['house','info-circle','card-checklist'],
-#     menu_icon='cast', default_index=0, orientation='horizontal')
+# Display the Navigation Bar
+nav_bar_selected = option_menu(None, config.APP_PAGES,
+    icons=['house','info-circle','card-checklist'],
+    menu_icon='cast', default_index=0, orientation='horizontal')
 
 
-# # Display the selected page
-# if nav_bar_selected == "Home":
-#     sched.display()
-# elif nav_bar_selected == "About":
-#     about.display()
-# elif nav_bar_selected == 'Feedback':
-#     feedback.display()
+# Display the selected page
+if nav_bar_selected == "Home":
+    pass # sched.display()
+elif nav_bar_selected == "About":
+    pass # about.display()
+elif nav_bar_selected == 'Feedback':
+    pass # feedback.display()
 
 
 
